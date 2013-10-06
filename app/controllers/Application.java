62e344e6b955d3a958468dcbb9b728ba5560857c
Edward Gong <edwardgong12@gmail.com>
2013-10-06 14:13:18 -0400

    Add non-scala html files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
-import views.html.*;
-
+/**
+ * The API available in the play.api package (such as play.api.mvc)
+ * is reserved for Scala developers. As a Java developer, look at play.mvc.
+ * @author Edward Gong
+ *
+ */
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		System.out.println("Playing around!");
+		return ok(index.render("Your new application is ready."));
+	}
 
 }
